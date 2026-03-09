e918f5ec2218078bc8375415e9a789e2619d11bd
cyber-junkie9 <50986901+cyber-junkie9@users.noreply.github.com>
2026-03-09 16:56:21 +0530

    Update udp_component.cpp

--- esphome/components/udp/udp_component.cpp
+++ esphome/components/udp/udp_component.cpp
@@ -145,9 +145,11 @@ void UDPComponent::dump_config() {
 void UDPComponent::send_packet(const uint8_t *data, size_t size) {
 #if defined(USE_SOCKET_IMPL_BSD_SOCKETS) || defined(USE_SOCKET_IMPL_LWIP_SOCKETS)
   for (const auto &saddr : this->sockaddrs_) {
-    auto result = this->broadcast_socket_->sendto(data, size, 0,
-                                                  (const struct sockaddr *) &saddr,
-                                                  sizeof(saddr));
+    const struct sockaddr *addr = reinterpret_cast<const struct sockaddr *>(&saddr);
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    if (addr->sa_family == AF_INET6)
+      addr_len = sizeof(struct sockaddr_in6);
+    auto result = this->broadcast_socket_->sendto(data, size, 0, addr, addr_len);
     if (result < 0)
       ESP_LOGW(TAG, "sendto() error %d", errno);
   }
